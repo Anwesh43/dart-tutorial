631a93754d8e32ccb0bfa5a9702c8672341d63ac
Anwesh43 <anweshthecool0@gmail.com>
2020-08-09 22:25:06 +0530

    adding Delay demo

--- AsyncDemo.dart
+++ AsyncDemo.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 
+Future<String> delayedDemo({int n = 1}) async => Future.delayed(Duration(seconds : n), () => 'delayed hello after $n seconds');
 Future<String> demo() async =>  'hello';
 Future  main() async {
     String k = await demo();
     print("k is $k");
+    delayedDemo().then(print);
+    String delayedString = await delayedDemo(n : 2);
+    print(delayedString);
 }
