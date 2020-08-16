84111b1fe71d024c4af2390920faec8c12685857
Anwesh43 <anweshthecool0@gmail.com>
2020-08-16 18:26:49 +0530

    using iterators and generators

--- practicetillnow.dart
+++ practicetillnow.dart
@@ -9,8 +9,35 @@ int add(int a, int b, [int c]) {
 int operate({int a, int b = 20, int c = 10, Function cb}) {
     return cb(a, b, c);
 }
-void main() {
+
+Iterable getFirstNaturalNumbers(int n, int k) sync* {
+    int i = k;
+    while (i < n) {
+        yield i;
+        i+=k;
+    }
+}
+
+Stream<int> getFirstNaturalStram(int n, int k) async* {
+    int i = k;
+    while (i < n) {
+        yield i;
+        i += k;
+    }
+
+}
+
+Future main() async {
+    getFirstNaturalStram(20, 2).listen((i) => print('listening to stream for $i'));
     print('the sum of 10, 20, 30 = ${add(10, 20, 30)}');
     print('the sum of 10, 20 is ${add(10, 20)}');
     print('operation ${operate(a : 6, cb : add)}');
+    for (int i in getFirstNaturalNumbers(20, 3)) {
+        print('current is $i');
+    }
+    await for (int i in getFirstNaturalStram(20, 5)) {
+        print('current in stream $i');
+    }
 }
+
+
