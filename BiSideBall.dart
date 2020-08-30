c58faf1d195e0941b9169d73049f6ce26c1afbdb
Anwesh43 <anweshthecool0@gmail.com>
2020-08-30 21:26:26 +0530

    adding State class

--- BiSideBall.dart
+++ BiSideBall.dart
@@ -1,6 +1,25 @@
 import 'dart:html';
 import 'dart:async';
 
+final int parts = 2;
+final double scGap = 0.02 / parts;
+
+class State {
+
+   double scale = 0.0;
+   
+   void update(Function cb) {
+      scale += scGap;
+      if (scale > 1) {
+          scale = 0; 
+          cb();
+      }
+   }
+
+   void startUpdating(Function cb) {
+      cb();
+   }
+}
 
 void main() {
 
