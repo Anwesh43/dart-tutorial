c94e9e6012fb08fc0f467abc678b7d946d3ea6bf
Anwesh43 <anweshthecool0@gmail.com>
2020-09-07 02:53:58 +0530

    adding State for SquareToDart

--- SquareToCircle.dart
+++ SquareToCircle.dart
@@ -1,5 +1,34 @@
 import 'dart:html';
 import 'dart:async';
+import 'dart:math';
+
+const double scGap = 0.01;
+
+double absolute(double a) {
+    return a >= 0? a: -a;
+}
+class State {
+
+    double scale = 0;
+    double dir = 0;
+    double prevScale = 0;
+    
+    void update(Function cb) {
+        this.scale += scGap * this.dir;
+        if (absolute(scale - prevScale) > 1) {
+            scale = prevScale + dir;
+            dir = 0;
+            prevScale = scale; 
+            cb();
+        }
+    }
+
+    void startUpdating(Function cb) {
+        if (dir == 0) {
+            dir = 1 - 2 * this.prevScale;
+        }
+    }
+}
 
 void main() {
 
