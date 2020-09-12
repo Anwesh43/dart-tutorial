2c6f705e2bd6b704d1690907408ba583105c1ddc
Anwesh43 <anweshthecool0@gmail.com>
2020-09-13 00:38:50 +0530

    adding State class

--- BouncyLineBall.dart
+++ BouncyLineBall.dart
@@ -1,6 +1,21 @@
 import 'dart:html';
 import 'dart:async';
 
+const double scGap = 0.02;
+
+class State {
+    
+    double scale = 0.0;
+
+    void update(Function cb) {
+        this.scale += scGap;
+        if (this.scale > 1) {
+            this.scale = 0.0;
+            cb();
+        } 
+    }
+}
+
 void main() {
 
 }
