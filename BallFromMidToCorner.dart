b419ee0ca7b461cc07b495518c2c96ed40ad8903
Anwesh43 <anweshthecool0@gmail.com>
2020-09-21 20:39:18 +0530

    adding BallFromMidToCorner class

--- BallFromMidToCorner.dart
+++ BallFromMidToCorner.dart
@@ -1,7 +1,13 @@
 import 'dart:html';
 import 'dart:async';
+import 'dart:math';
 
 const double scGap = 0.02;
+int w = window.innerWidth;
+int h = window.innerHeight; 
+const int delay = 20;
+const int sizeFactor = 8;
+double size = h / sizeFactor;
 
 class State {
 
@@ -38,9 +44,41 @@ class Animator {
             timer.cancel();
         }
     }
+}
 
+class BallFromMidToCorner {
 
+    DivElement ball;
+    State state;
+
+    void init() {
+        ball = document.createElement('div');
+         
+        state = new State();
+        ball.style.borderRadius = "50%";
+        ball.style.background = "indigo";
+        ball.style.width = "${size}px";
+        ball.style.height = "${size}px";
+        ball.style.position = "absolute";
+        ball.style.top = "${w / 2 - size / 2}px";
+        ball.style.left = "${h / 2 - size / 2}px";
+        document.body.append(this.ball);
+    }
+
+    void update(Function cb) {
+        state.update(cb);
+        double x = (w / 2 - size / 2) * (1 - sin(pi * state.scale));
+        double y = (h / 2 - size / 2) * (1 - sin(pi * state.scale));
+        ball.style.left = "${x}px";
+        ball.style.top = "${y}px";
+    }
+
+    void handleTap(Function cb) {
+        ball.onClick.listen((e) {
+            cb();
+        });
+    }
 }
 void main() {
-
+  
 }
